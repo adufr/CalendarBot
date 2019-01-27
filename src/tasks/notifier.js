f42a2d9af6bc674222cf17f7fdc238278a3deb5b
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-01-27 21:52:36 +0100

    fix: finished notifications

--- src/tasks/notifier.js
+++ src/tasks/notifier.js
@@ -15,23 +15,34 @@ module.exports = class extends Task {
       const channelId = guild.settings.channels.notifications
       if (!channelId) return
       
-      // gets tasks for tomorrow
-      const embed = this.client.funcs.getNotificationEmbed(this.client, message)
-      if (!embed) return
-
       // if the channel is valid
       const channel = guild.channels.find(channel => channel.id === channelId)
       if (!channel) return
+
+      // gets tasks for tomorrow
+      const embed = this.client.funcs.getNotificationEmbed(this.client, channel)
+      if (!embed) return
       
       // if bot's last msg is already a notification: delete & post new one
       // otherwise, do nothing but posting the new one
-      const lastMessage = channel.messages.last()
-      if (lastMessage && lastMessage.author.id === this.client.user.id) {
-        lastMessage.delete()
-      } 
+      channel.messages.fetch({limit: 2}).then(messages => {
+        messages.forEach(msg => {
+          if (msg.author.id === this.client.user.id && (msg.embeds || msg.mentions.roles)) {
+            msg.delete()
+          }
+        })
+      })
+      
+      const roleId = guild.settings.roles.notify
+      if (roleId) {
+        const role = guild.roles.find(role => role.id === roleId)
+        channel.send(`${role.toString()}`)
+      } else {
+        channel.send(`Notification time ! (vous pouvez configurer un rôle à tag via \`%conf role notif @notification\`)`)
+      }
 
       channel.send(embed)
-      this.client.console.log(`Sent a notification for guild ${channel.guild.name} (${channel.guild.id})!`)
+      this.client.console.log(`Sent a notification for guild ${channel.guild.name} (${channel.guild.id})`)
     })
   }
 
